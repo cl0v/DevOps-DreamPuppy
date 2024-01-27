a8706079d927c5456f568f0a42f200c3da24196b
Marcelo Viana <marcelofv12@hotmail.com>
2024-01-27 14:02:34 -0300

    insomnia working and curl for localhost
    curl localhost:8080

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -18,12 +18,25 @@ kubectl create secret docker-registry regcred --docker-server=$DOCKER_REGISTRY_S
 # Install Ingress-controller-nginx [https://kubernetes.github.io/ingress-nginx/deploy/#rancher-desktop]
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
 
-# Aguarda subir os container responsáveis pelo ingress:
-kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+# Interval to inject commands after setup
+sleep 10
+
+# Wait for the Ingress controller to be ready
+echo "Waiting for Ingress controller to be ready..."
+while ! kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s; do
+    echo "Ingress controller is not ready yet. Retrying in 10 seconds..."
+    sleep 10
+done
+
+echo "Ingress controller is now ready."
 
 # Cria um recurso ingress
-kubectl create ingress demo-localhost --class=nginx --rule="demo.localdev.me/*=demo:80"
+kubectl apply -f cluster/ingress.yaml
 
 # Direciona a porta para localhost:8080 [WARNING: Port-forwarding is not for a production environment use-case]
 kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
+
+# Verfica conexão com o pod
+echo "Validando conexão: "
+curl --resolve dev.dreampuppy.com:8080:127.0.0.1 http://dev.dreampuppy.com:8080
 
