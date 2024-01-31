8f4446a283f6296b023b8ba65705121596b075ef
Marcelo Viana <marcelofv12@hotmail.com>
2024-01-31 16:23:56 -0300

    fix

--- lab/export-backup-sql.sh
+++ lab/export-backup-sql.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Required var-envs
-POD_ID=$(kubectl get pods -o jsonpath='{.items[0].metadata.name}' | grep postgres)
+POD_ID=$(kubectl get pods -o name | grep postgres | cut -d'/' -f2)
 DB_USER=postgres
 DB_NAME=database
 
+echo "> Exportando dados de backup"
 # Exporta o banco de dados
 kubectl exec -i $POD_ID -- pg_dump -U $DB_USER $DB_NAME > ../backup.sql
